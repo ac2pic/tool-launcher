fc747641b11281bd6922be00a8a586655abd9394
ac2pic <ac2pic@gmail.com>
2020-02-22 20:49:07 -0600

    Renamed ToolsApi to ToolsCommunicationApi + added features

--- tools-launcher/src/tool/api/tools.js
+++ tools-launcher/src/tool/api/tools.js
@@ -1,16 +1,46 @@
-const path = require('path');
+import ToolCommunicationClient from './client.js';
 
-export default class ToolsApi {
+const topic = {};
+
+export default class ToolsCommunicationApi {
     constructor() {
     }
 
-
-    getRealAssetsPath() {
-        return path.join(nw.App.startPath, 'assets/');
+    addTopic(name) {
+        if (this.hasTopic(name)) {
+            console.warn(`Topic ${name} already exists.`);
+            return;
+        }
+        
+        topic[name] = [];
     }
 
-    getChromeAssetsPath() {
-        return path.posix.join(location.origin, 'assets/');
+    subscribeToTopic(name, subscriber) {
+        if (!(subscriber instanceof ToolCommunicationClient)) {
+            throw Error(`Subscriber must be an instance of ToolCommunicationClient`);
+        }
+
+
+        if (!this.hasTopic(name)) {
+            this.addTopic(name);
+        }
+
+        topic[name].push(subscriber);
     }
 
+    hasTopic(name) {
+        return Array.isArray(topic[name]);
+    }
+
+    emit(topicName, message) {
+        if (this.hasTopic(topicName)) {
+            for (const subscriber of topic[topicName]) {
+                const clonedMessage = message.clone();
+
+                clonedMessage.setConsumer(subscriber);
+                
+                subscriber.onMessage(clonedMessage);
+            }
+        }
+    }
 }
