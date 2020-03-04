089e8aff235a5151b92cdb5f7e86fee6f84d54eb
ac2pic <ac2pic@gmail.com>
2020-03-04 17:15:40 -0600

    Added NwJsToolFrameManager

---
+++ tools-launcher/src/tool/frame-manager/nw.js
@@ -0,0 +1,48 @@
+import BaseToolFrameManager from "./base.js";
+
+export default class NwJsToolFrameManager extends BaseToolFrameManager {
+
+    async open() {
+        const config = {
+            id: this.id,
+            icon: this.icon
+        };
+        return new Promise((resolve, reject) => {
+            nw.Window.open(this.src, config, (nwWindow) => {
+                this.set(nwWindow);
+                this.addNwListeners();
+                resolve(this);
+            });
+        });
+    }
+    
+    addNwListeners() {
+        const frame = this.get();
+        let alreadyLoaded = false;
+        const window = this.getWindow();
+        window.addEventListener('load', () => {
+            // if it failed
+            debugger;
+            if (!alreadyLoaded) {
+                alreadyLoaded = true;
+                this.onLoaded(frame, window.location.origin !== "null");
+            }
+        });
+
+        frame.on('loaded', () => {
+            // if it successed
+            if (!alreadyLoaded) {
+                alreadyLoaded = true;
+                this.onLoaded(frame, window.location.origin !== "null");
+            }
+        });
+
+        frame.on('closed', () => {
+            this.onClose(frame);
+        });
+    }
+
+    close() {
+        this.get().close(true);
+    }
+}
