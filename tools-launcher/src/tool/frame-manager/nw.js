5ee6188b1439f830e2d45dcac1d672dabbe39bf3
ac2pic <ac2pic@gmail.com>
2020-03-06 14:35:18 -0600

    Fixed reloading issue in nwjs frame manager implementation

--- tools-launcher/src/tool/frame-manager/nw.js
+++ tools-launcher/src/tool/frame-manager/nw.js
@@ -16,27 +16,32 @@ export default class NwJsToolFrameManager extends BaseToolFrameManager {
             });
         });
     }
-    
-    addNwListeners() {
-        const frame = this.get();
-        let alreadyLoaded = false;
-        const window = this.getWindow();
-        window.addEventListener('load', () => {
-            // if it failed
-            if (!alreadyLoaded) {
-                alreadyLoaded = true;
-                this.onLoaded(this, window.location.origin !== "null");
-            }
-        });
 
-        window.addEventListener('beforeunload', function() {
-            alreadyLoaded = false;
-        });
+    addNwListeners() {
+        let alreadyLoaded = false;
+        const repeatedListeners = () => {
+            const window = this.getWindow();
+            window.addEventListener('load', () => {
+                // if it failed
+                if (!alreadyLoaded) {
+                    alreadyLoaded = true;
+                    this.onLoaded(this, window.location.origin !== "null");
+                }
+            });
+
+            window.addEventListener('beforeunload', function() {
+                alreadyLoaded = false;
+            });
+        };
+        repeatedListeners();
+
+        const frame = this.get();
 
         frame.on('loaded', () => {
             // if it succeeded
             if (!alreadyLoaded) {
                 alreadyLoaded = true;
+                repeatedListeners();
                 this.onLoaded(this, window.location.origin !== "null");
             }
         });
