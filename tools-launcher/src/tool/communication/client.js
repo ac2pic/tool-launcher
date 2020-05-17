9070df427512714f2c3593f150823738b1909d54
ac2pic <ac2pic@gmail.com>
2020-05-17 11:15:57 -0500

    Added subscribeTo and unsubscribeFrom to Tool.Communication.Client

--- tools-launcher/src/tool/communication/client.js
+++ tools-launcher/src/tool/communication/client.js
@@ -14,16 +14,40 @@ export default class Client {
     /**
      * 
      * @param {string} topic 
-     * @param {ToolMessage} msg 
+     * @param {Tool.Communication.Message} msg 
      */
     emit(topic, msg) {
         msg.setProvider(this);
         Client.topicInstance.emit(topic, msg);
     }
 
+    /**
+     * 
+     * @param {string} topic 
+     */
+    subscribeTo(topic) {
+        Client.topicInstance.subscribeTo(topic, this);
+    }
 
+    /**
+     * 
+     * @param {string} topic 
+     */
+    unsubscribeFrom(topic) {
+        Client.topicInstance.unsubscribeFrom(topic, this);
+    }
+
+    /**
+     * 
+     * @param {string} topic 
+     * @param {Tool.Communication.Message} message 
+     */
     onMessage(topic, message) { }
 
-
+    /**
+     * 
+     * @param {string} topic 
+     * @param {Tool.Communication.Message} message 
+     */
     onReply(topic, message) { }
 }
